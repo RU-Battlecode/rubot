09c6de5c4b803f3e028728a2d0d8c19f294759a3
imachumphries <imachumphries@me.com>
2016-10-18 01:55:47 -0400

    Remove unneeded constant
    Use Battlecode.GameConstants.java

--- Constants.java
+++ Constants.java
@@ -10,5 +10,4 @@ public class Constants {
     final Direction[] DIRECTIONS = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
             Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 
-    final int SOLDIER_RANGE = 13;
 }
