bc33a42876f99bbcf9b7c403d2cbf95301b91b4c
imachumphries <imachumphries@me.com>
2016-10-24 16:03:15 -0400

    Revert

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -8,48 +8,40 @@ import rubot.src.logic.ScoutLogic;
 import rubot.src.logic.SoldierLogic;
 import rubot.src.logic.TurretLogic;
 import rubot.src.logic.ViperLogic;
-import rubot.src.test.Testing;
 
 /**
  * Created by Travis on 9/26/2016.
  */
 public class RobotPlayer {
 
-    public static void run(RobotController _rc){
-    	if (Testing.TEST_MODE) {
-    		Testing.runAllTests();
-    	} else {
-    		startNewBot(_rc);
-    	}  
-    }
-    
-    private static void startNewBot(RobotController _rc) {
-    	 RobotLogic logic = null;
-         switch (_rc.getType()){
-             case ARCHON:
-                 logic = new ArchonLogic();
-                 break;
-             case SCOUT:
-             	logic = new ScoutLogic();
-                 break;
-             case SOLDIER:
-             	logic = new SoldierLogic();
-                 break;
-             case GUARD:
-             	logic = new GuardLogic();
-                 break;
-             case VIPER:
-             	logic = new ViperLogic();
-                 break;
-             case TURRET:
-             case TTM:
-             	logic = new TurretLogic();
-                 break;
-             default:
-                 System.out.println("Missing logic");
-         }
+	public static void run(RobotController _rc) {
+		RobotLogic logic = null;
+		switch (_rc.getType()) {
+		case ARCHON:
+			logic = new ArchonLogic();
+			break;
+		case SCOUT:
+			logic = new ScoutLogic();
+			break;
+		case SOLDIER:
+			logic = new SoldierLogic();
+			break;
+		case GUARD:
+			logic = new GuardLogic();
+			break;
+		case VIPER:
+			logic = new ViperLogic();
+			break;
+		case TURRET:
+		case TTM:
+			logic = new TurretLogic();
+			break;
+		default:
+			System.out.println("Missing logic");
+		}
 
-         logic.setRc(_rc);
-         logic.run();
-    }
+		logic.setRc(_rc);
+		logic.run();
+	}
+	
 }
